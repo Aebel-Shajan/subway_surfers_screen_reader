e1320a40dbf644e3a14e11e30e2d5d369fd392bb
Aebel Shajan <aebelshajan@gmail.com>
2023-12-31 11:03:15 +0000

    refactored code in sidePanel.js and changed style a bit

--- sidePanel.js
+++ sidePanel.js
@@ -1,44 +1,74 @@
+// DOM Element Selectors
+const elements = {
+	readButton: document.querySelector("#read-website"),
+	playPause: document.querySelector("#play-pause-toggle"),
+	stop: document.querySelector("#stop"),
+	videoPlayer: document.querySelector("#video"),
+	previewText: document.querySelector("#preview-text"),
+	textOverlay: document.querySelector("#text-overlay"),
+};
 async function sendMessageToActiveTab(message) {
 	const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-	// if we dont want the user to refresh the page 
-	// await chrome.tabs.reload(tab.id);
-	// // Wait for the tab to finish loading
-	// await new Promise(resolve => {
-	//   const listener = (tabId, changeInfo) => {
-	//     if (tabId === tab.id && changeInfo.status === 'complete') {
-	//       chrome.tabs.onUpdated.removeListener(listener);
-	//       resolve();
-	//     }
-	//   };
-	//   chrome.tabs.onUpdated.addListener(listener);
-	// });
 	const response = await chrome.tabs.sendMessage(tab.id, { ...message, sender: "side-panel" });
 	return response
 }
-
-document.querySelector("#read-website").addEventListener('click', async () => {
-	try {
-		const response = await sendMessageToActiveTab({ action: "read-website" });
-		console.log("Response message: ", response);
-		let scrapedContent = response.content;
-		document.querySelector("#preview-text").value = cleanUpText(scrapedContent.textContent);
-	} catch (error) {
-		document.querySelector("#preview-text").value = ":( no worky, sorry. Try refreshing the current page and press read website again";
-		console.log('An error occurred:', error);
-	}
-
-})
-
-let playPauseButton = document.querySelector("#play-pause-toggle");
-let stopButton = document.querySelector("#stop");
-let videoPlayer = document.querySelector("#video");
-let previewText = document.querySelector("#preview-text");
-let readButton = document.querySelector("#read-website");
 function setButtonState(button, state) {
 	button.className = state;
 	button.textContent = state.charAt(0).toUpperCase() + state.slice(1);
 	return button
 }
+
+// tts functions
+function getWordAtIndex(text, index) {
+	let words = text.split(/ |\n/);
+	let accumulatedLength = 0;
+	for (let i = 0; i < words.length; i++) {
+		accumulatedLength += words[i].length;
+		// Add 1 for the space character
+		if (i !== words.length - 1) {
+			accumulatedLength += 1;
+		}
+		if (index < accumulatedLength) {
+			return words[i];
+		}
+	}
+	return null;
+}
+function cleanUpText(text) {
+	if (text.length > 32768) {
+		text = text.slice(0, 32768) // max lenght that tts allows
+	}
+	return text
+		// remove spaces before and after
+		.trim()
+		// remove spaces between new lines
+		.replace(/ +\n/g, '\n')
+		// condense multiple new lines into two
+		.replace(/\n+/g, '\n\n')
+		// add space to end of sentences
+		.replace(/(?<=[A-Za-z0-9])\.(?=[A-Z])/g, '. ');
+}
+function ttsEventHandler(event, words) {
+	console.log('Event ' + event.type + ' at position ' + event.charIndex);
+	elements.textOverlay.innerText = getWordAtIndex(words, event.charIndex);
+	switch (event.type) {
+		case "start":
+			elements.previewText.setAttribute("disabled", "disabled");
+			elements.readButton.setAttribute("disabled", "disabled");
+			break;
+		case "end":
+		case "interrupted":
+			elements.previewText.removeAttribute("disabled");
+			elements.readButton.removeAttribute("disabled");
+			elements.playPause = setButtonState(elements.playPause, "play");
+			stopVideoPlayer(elements.videoPlayer);
+			break;
+		default:
+			break;
+	}
+}
+
+// Video player functions
 function postMessageToVideo(videoPlayer, message) {
 	videoPlayer.contentWindow.postMessage(JSON.stringify(message), "*");
 }
@@ -77,85 +107,45 @@ function seekVideoToRandomTime(videoPlayer) {
 	return videoPlayer
 }
 
-playPauseButton.addEventListener("click", async () => {
+// Event listeners
+elements.playPause.addEventListener("click", async () => {
 	const isSpeaking = await chrome.tts.isSpeaking();
 	if (isSpeaking) {
-		if (playPauseButton.className === "play") {
-			playPauseButton = setButtonState(playPauseButton, "pause")
+		if (elements.playPause.className === "play") {
+			elements.playPause = setButtonState(elements.playPause, "pause")
 			chrome.tts.resume();
-			playVideoPlayer(videoPlayer);
+			playVideoPlayer(elements.videoPlayer);
 		} else {
-			playPauseButton = setButtonState(playPauseButton, "play")
+			elements.playPause = setButtonState(elements.playPause, "play")
 			chrome.tts.pause();
-			pauseVideoPlayer(videoPlayer);
+			pauseVideoPlayer(elements.videoPlayer);
 		}
 	} else {
 		chrome.tts.stop();
-		videoPlayer = seekVideoToRandomTime(videoPlayer);
-		playPauseButton = setButtonState(playPauseButton, "pause");
-		let preview = document.querySelector("#preview-text");
-		let words = preview.value;
+		elements.videoPlayer = seekVideoToRandomTime(elements.videoPlayer);
+		elements.playPause = setButtonState(elements.playPause, "pause");
+		let words = elements.previewText.value;
 		chrome.tts.speak(words, {onEvent: (event) => {
 			ttsEventHandler(event, words)
 		}})
 	}
 });
 
-function ttsEventHandler(event, words) {
-	console.log('Event ' + event.type + ' at position ' + event.charIndex);
-	document.querySelector("#text-overlay").innerText = getWordAtIndex(words, event.charIndex);
-	switch (event.type) {
-		case "start":
-			previewText.setAttribute("disabled", "disabled");
-			readButton.setAttribute("disabled", "disabled");
-
-			break;
-		case "end":
-		case "interrupted":
-			previewText.removeAttribute("disabled");
-			readButton.removeAttribute("disabled");
-			playPauseButton = setButtonState(playPauseButton, "play");
-			stopVideoPlayer(videoPlayer);
-			break;
-		default:
-			break;
-	}
-}
-
-stopButton.addEventListener("click", () => {
+elements.stop.addEventListener("click", () => {
 	chrome.tts.stop();
-	playPauseButton = setButtonState(playPauseButton, "play");
-	stopVideoPlayer(videoPlayer);
+	elements.playPause = setButtonState(elements.playPause, "play");
+	stopVideoPlayer(elements.videoPlayer);
 })
 
-
-function getWordAtIndex(text, index) {
-	let words = text.split(/ |\n/);
-	let accumulatedLength = 0;
-	for (let i = 0; i < words.length; i++) {
-		accumulatedLength += words[i].length;
-		// Add 1 for the space character
-		if (i !== words.length - 1) {
-			accumulatedLength += 1;
-		}
-		if (index < accumulatedLength) {
-			return words[i];
-		}
+elements.readButton.addEventListener('click', async () => {
+	try {
+		const response = await sendMessageToActiveTab({ action: "read-website" });
+		console.log("Response message: ", response);
+		const scrapedContent = response.content;
+		elements.previewText.value = cleanUpText(scrapedContent.textContent);
+	} catch (error) {
+		elements.previewText.value = ":( no worky, sorry. Try refreshing the current page and press read website again";
+		console.log('An error occurred:', error);
 	}
-	return null;
-}
 
-function cleanUpText(text) {
-	if (text.length > 32768) {
-		text = text.slice(0, 32768) // max lenght that tts allows
-	}
-	return text
-		// remove spaces before and after
-		.trim()
-		// remove spaces between new lines
-		.replace(/ +\n/g, '\n')
-		// condense multiple new lines into two
-		.replace(/\n+/g, '\n\n')
-		// add space to end of sentences
-		.replace(/(?<=[A-Za-z0-9])\.(?=[A-Z])/g, '. ');
-}
+})
