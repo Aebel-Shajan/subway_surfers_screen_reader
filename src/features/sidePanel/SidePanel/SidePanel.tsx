891c254fb7feceab761d146c1dc2514f162fcdf7
aebel-shajan <aebelshajan@gmail.com>
2024-10-27 13:05:47 +0000

    Add resizeable panels and add youtube compoent to sidepanel.
    I can't believe its this easy in react 😭. I spent like 3 whole weeks trying to get this to work in normal js.

--- src/features/sidePanel/SidePanel/SidePanel.tsx
+++ src/features/sidePanel/SidePanel/SidePanel.tsx
@@ -1,18 +1,28 @@
 import styles from "./SidePanel.module.css"
 import logo from "@/assets/genz-screenreader-logo.png"
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
+import { MdDragHandle } from "react-icons/md";
+// https://github.com/tjallingt/react-youtube
+import YouTube from "react-youtube";
+
 
 const SidePanel = () => {
-    return ( 
-        <div className={styles.container}>
-            <header className={styles.header}>
-                <img src={logo} />
-                <span>Subway surfers sidepanel</span>
-            </header>
-            <main>
-
-            </main>
-        </div>
-     );
+  return (
+      <PanelGroup className={styles.container} direction="vertical">
+        <Panel maxSize={75} className={styles.upperPanel}>
+          <header>
+            <img src={logo} />
+            <span>Subway surfers sidepanel</span>
+          </header>
+        </Panel>
+        <PanelResizeHandle className={styles.resizeHandle}>
+          <MdDragHandle />
+        </PanelResizeHandle>
+        <Panel>
+          <YouTube videoId={"BkWT66jE8Hs"} className={styles.youtube}/>
+        </Panel>
+      </PanelGroup>
+  );
 }
- 
+
 export default SidePanel;
