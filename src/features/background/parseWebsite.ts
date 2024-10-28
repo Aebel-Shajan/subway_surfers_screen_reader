c63d25b4003c4b6d40e6135b76161007724a4280
aebel-shajan <aebelshajan@gmail.com>
2024-10-28 13:30:33 +0000

    Fix document modified when reading page.

--- src/features/background/parseWebsite.ts
+++ src/features/background/parseWebsite.ts
@@ -1,6 +1,11 @@
 import { Readability } from "@mozilla/readability";
 
-const websiteContent = new Readability(document).parse();
-if (websiteContent) {
-    chrome.runtime.sendMessage({ text: websiteContent.textContent });
+// In braces because variable stays persistent.
+// When I execute this script multiple times, the websiteContent stays persistent.
+{
+    const clonedDocument = document.cloneNode(true) as Document;
+    const websiteContent = new Readability(clonedDocument).parse();
+    if (websiteContent) {
+        chrome.runtime.sendMessage({ text: websiteContent.textContent });
+    }
 }
