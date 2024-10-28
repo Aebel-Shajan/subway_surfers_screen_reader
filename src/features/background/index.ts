c34270379cf6ac2b3e1aa682409a5f5c98e8e8cc
aebel-shajan <aebelshajan@gmail.com>
2024-10-28 11:55:18 +0000

    Add logic to run screen reader when context menu clicked. Refactor screen reader logic in SidePanel.tsx.

--- src/features/background/index.ts
+++ src/features/background/index.ts
@@ -1,4 +1,93 @@
+import { Readability } from "@mozilla/readability";
+
+let sidePanelPort: chrome.runtime.Port|null = null;
+let onSidePanelConnect = () => {};
+
 // Open sidepanel on action icon click
 chrome.sidePanel
 	.setPanelBehavior({ openPanelOnActionClick: true })
 	.catch((error) => console.error(error));
+
+// Stop tts when sidepanel is closed
+chrome.runtime.onConnect.addListener(function (port: chrome.runtime.Port) {
+	if (port.name === 'mySidepanel') {
+		console.log("sidepanel port open!")
+		sidePanelPort = port
+		onSidePanelConnect()
+		onSidePanelConnect = () => {}
+		port.onDisconnect.addListener(async () => {
+			console.log('Sidepanel port closed!.');
+			sidePanelPort = null
+			onSidePanelConnect = () => {}
+			chrome.tts.stop();
+		});
+	}
+});
+
+// Add context menu for reading website
+chrome.runtime.onInstalled.addListener(function () {
+	chrome.contextMenus.create({
+		title: "Read website (Dev)",
+		id: "read-website",
+		contexts: ["page"]
+	});
+	chrome.contextMenus.create({
+		title: "Read selection (Dev): %s",
+		id: "read-selection",
+		contexts: ["selection"]
+	});
+})
+// 
+chrome.contextMenus.onClicked.addListener(
+	async (info, tab) => {
+		if (!tab) return
+		if (!tab.id) return
+		console.log("checks passed")
+		switch(info.menuItemId) {
+			case "read-website":
+				await chrome.sidePanel.open({ windowId: tab.windowId });
+				if (sidePanelPort) {
+					readWebsite(tab.id, sidePanelPort);
+				} else {
+					const tabId = tab.id;
+					onSidePanelConnect = () => {
+						if (!sidePanelPort) return
+						readWebsite(tabId, sidePanelPort);
+					}
+				}
+				break;
+			case "read-selection":
+				if (!info.selectionText) return;
+				console.log("Selection message send")
+				await chrome.sidePanel.open({ windowId: tab.windowId });
+				if (sidePanelPort) {
+					readSelection(info.selectionText, sidePanelPort);
+				} else {
+					onSidePanelConnect = () => {
+						if (!sidePanelPort) return
+						readSelection(info.selectionText? info.selectionText : "", sidePanelPort)
+					};
+				}
+				break;
+			default:
+				break;
+		}
+	}
+)
+
+function readSelection(selection: string, sidePanelPort: chrome.runtime.Port) {
+	sidePanelPort.postMessage({text: selection})
+}
+
+function readWebsite(tabId: number, sidePanelPort: chrome.runtime.Port) {
+	chrome.scripting.executeScript({
+		target: { tabId: tabId },
+		func: () => {
+			const websiteContent = new Readability(document).parse();
+			if (websiteContent) {
+				sidePanelPort.postMessage({ text: websiteContent.textContent });
+			}
+		},
+		args: []
+	})
+}
--- src/features/sidePanel/SidePanel/SidePanel.tsx
+++ src/features/sidePanel/SidePanel/SidePanel.tsx
@@ -3,8 +3,8 @@ import logo from "@/assets/genz-screenreader-logo.png"
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
 import { MdDragHandle } from "react-icons/md";
 import { FaPause, FaPlay, FaStop, FaTrash } from "react-icons/fa6";
-import { useRef, useState } from "react";
-import { delay, getWordAtIndex } from "@/utils/utils";
+import { useEffect, useRef, useState } from "react";
+import { cleanUpText, delay } from "@/utils/utils";
 import Youtube from "./Youtube/Youtube";
 import { pauseVideoPlayer, playVideoPlayer, seekVideoToRandomTime, stopVideoPlayer } from "@/utils/youtubeHelpers";
 
@@ -28,14 +28,15 @@ const SidePanel = () => {
    * I end the video here when the tts fails/ends/stops.
    * 
    * @param event Chrome tts event
+   * @param textBeingRead Full text that is currently bein
    * @remark  https://developer.chrome.com/docs/extensions/reference/api/tts#listen_to_events
    */
-  function ttsEventHandler(event: chrome.tts.TtsEvent) {
+  function ttsEventHandler(event: chrome.tts.TtsEvent, textBeingRead: string) {
+    console.log(event)
     switch (event.type) {
       case "start":
         setHasStarted(true)
         setIsPlaying(true)
-        setCurrentWord(getWordAtIndex(inputText, 0))
         break;
       case "resume":
         setHasStarted(true)
@@ -43,8 +44,8 @@ const SidePanel = () => {
         break;
       case "sentence":
       case "word":
-        if (!event.charIndex) break
-        setCurrentWord(getWordAtIndex(inputText, event.charIndex))
+        if (event.charIndex===undefined|| event.length===undefined) break
+        setCurrentWord(textBeingRead.slice(event.charIndex, event.charIndex + event.length))
         break
       case "end":
       case "interrupted":
@@ -65,6 +66,45 @@ const SidePanel = () => {
   }
 
   /**
+   * Trigger the screen reader to run. The video is first set to a random time. Then we 
+   * play, then wait a bit then use the tts. We delay due to some buffering.
+   */
+  async function startScreenReader(textToRead: string) {
+    if (!youtubeRef.current) return 
+    seekVideoToRandomTime(youtubeRef.current, 300, 3600)
+    await delay(500)
+    playVideoPlayer(youtubeRef.current)
+    await delay(500)
+    chrome.tts.speak(textToRead, { onEvent: (event) => ttsEventHandler(event, textToRead) });
+  }
+
+
+  /**
+   * Pauses the screen reader and the YouTube video player.
+   */
+  function pauseScreenReader() {
+    if (!youtubeRef.current) return 
+    pauseVideoPlayer(youtubeRef.current)
+    chrome.tts.pause();
+  }
+
+  /**
+   * Resumes the screen reader. First play the video, (wait a bit cus it buffers)
+   * then resume the tts.
+   * 
+   */
+  async function resumeScreenReader() {
+    if (!youtubeRef.current) return 
+    playVideoPlayer(youtubeRef.current)
+    await delay(500)
+    chrome.tts.resume();
+  }
+
+  function stopScreenReader() {
+    chrome.tts.stop();
+  }
+
+  /**
    * Reset input text when trash button pressed.
    */
   function handleClearText() {
@@ -74,49 +114,57 @@ const SidePanel = () => {
   /**
    * Stops the text-to-speech (TTS) engine.
    *
-   * @Remark I have not stopped the video here because it is more robust to handle it
-   * in the ttsEventHandler. There it considers cases where it ends, interrupted, 
-   * cancelled or errored out.
    */
   function handleStop() {
-    chrome.tts.stop()
+    stopScreenReader()
   }
 
   /**
    * Handles the play/pause functionality.
    * 
-   * * If the tts is currently playing, it pauses both the video player and the TTS 
-   *  service.
-   * * If the tts has not started, it starts the video from a random time, waits
-   *   a while and then begins TTS.
-   * * If the tts is has started and is not currently playing, it starts the vid, waits,
-   *   then resumes tts.
-   * 
-   * @remark The reason for the wait is because the vid takes some time to load up.
-   * 
-   * 
    * @returns A promise that resolves when the play/pause actions are completed.
    */
   async function handlePlayPause() {
-    if (!youtubeRef.current) return 
     if (isPlaying) {
-      pauseVideoPlayer(youtubeRef.current)
-      chrome.tts.pause();
+      pauseScreenReader()
     } else {
       if (!hasStarted) {
-        seekVideoToRandomTime(youtubeRef.current, 300, 3600)
-        await delay(500)
-        playVideoPlayer(youtubeRef.current)
-        await delay(500)
-        chrome.tts.speak(inputText, { onEvent: ttsEventHandler });
+        startScreenReader(inputText)
       } else {
-        playVideoPlayer(youtubeRef.current)
-        await delay(500)
-        chrome.tts.resume();
+        resumeScreenReader()
       }
     }
   }
 
+  // Use effect hooks
+
+  // On initial render
+  useEffect(() => {
+    // Setup sidepanel port. keep it open by regularly sending messages
+    const port = chrome.runtime.connect({ name: 'mySidepanel' });
+    const intervalId = setInterval(() => {
+      port.postMessage({ info: "keeping connection open" });
+    }, 1000);
+    console.log("sidepanel port opened - from sidepanel")
+    port.onDisconnect.addListener(() => {
+      console.log("sidepanel port closed - from sidepanel")
+    })
+
+    // On message 
+    port.onMessage.addListener(async (message) => {
+      stopScreenReader()
+      if (!message.text) return
+      await delay(800) // The loading is artificial 🤭.
+      const textToRead = cleanUpText(message.text)
+      setInputText(textToRead)
+      startScreenReader(textToRead)
+    })
+
+    return () => {
+      clearInterval(intervalId);
+      port.disconnect();
+    }
+  }, [])
 
   return (
     <PanelGroup className={styles.container} direction="vertical">
