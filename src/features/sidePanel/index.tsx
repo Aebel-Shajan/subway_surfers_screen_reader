10440f04c5d3389e95b8c71ba351765bed48ee41
aebel-shajan <aebelshajan@gmail.com>
2024-10-30 15:36:38 +0000

    Add MantineProvider and import mantine css in index.tsx.

--- src/features/sidePanel/index.tsx
+++ src/features/sidePanel/index.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import SidePanel from './SidePanel/SidePanel'
 import './index.css'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <SidePanel />
+      <MantineProvider >
+        <SidePanel />
+      </MantineProvider>
   </StrictMode>,
 )
