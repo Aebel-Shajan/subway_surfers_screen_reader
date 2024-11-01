45e779eead1d02316c65dc1681d25de844c5cd94
aebel-shajan <aebelshajan@gmail.com>
2024-11-01 11:10:01 +0000

    Add fallback component in case of error.

--- src/features/sidePanel/index.tsx
+++ src/features/sidePanel/index.tsx
@@ -4,12 +4,17 @@ import SidePanel from './SidePanel/SidePanel'
 import './index.css'
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { ErrorBoundary } from "react-error-boundary";
+import Fallback from './Fallback/Fallback';
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ErrorBoundary
+      FallbackComponent={Fallback}>
       <MantineProvider >
         <SidePanel />
       </MantineProvider>
+    </ErrorBoundary>
   </StrictMode>,
 )
