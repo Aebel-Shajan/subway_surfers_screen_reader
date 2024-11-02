a95b8946934539822a2d02033e57440351f16f3d
aebel-shajan <aebelshajan@gmail.com>
2024-11-02 15:44:03 +0000

    Update styling of Youtube component.

--- src/components/Youtube/Youtube.tsx
+++ src/components/Youtube/Youtube.tsx
@@ -1,26 +1,33 @@
 import { forwardRef } from "react";
 import styles from "./Youtube.module.css";
 import getVideoId from 'get-video-id';
-import { DEFAULT_OPTIONS} from "@/constants";
+import { DEFAULT_OPTIONS } from "@/constants";
 
-interface YoutubeProps{
-    videoUrl: string
+interface YoutubeProps {
+  videoUrl: string
 }
 
-const Youtube = ({videoUrl}: YoutubeProps, ref: React.ForwardedRef<HTMLIFrameElement>) => {
-    let videoId: string | undefined = getVideoId(videoUrl).id
-    if (!videoId) {
-        videoId = getVideoId(DEFAULT_OPTIONS.videos[DEFAULT_OPTIONS.selectedVideo].url).id // never undefined
-    }
+const Youtube = ({ videoUrl }: YoutubeProps, ref: React.ForwardedRef<HTMLIFrameElement>) => {
+  let videoId: string | undefined = getVideoId(videoUrl).id
+  if (!videoId) {
+    videoId = getVideoId(DEFAULT_OPTIONS.videos[DEFAULT_OPTIONS.selectedVideo].url).id // never undefined
+  }
 
-    return (
-    <iframe 
-        className={styles.iframe}
-        ref={ref}
-        id="video" 
-        src={`https://www.youtube.com/embed/${videoId}?mute=1&enablejsapi=1`}
-    />
-    );
+  return (
+    <div 
+      className={styles.iframeContainerContainer}>
+      <div 
+        className={styles.iframeContainer}>
+        <iframe
+          className={styles.iframe}
+          ref={ref}
+          id="video"
+          src={`https://www.youtube.com/embed/${videoId}?mute=1&enablejsapi=1`}
+        />
+      </div>
+    </div>
+
+  );
 }
- 
+
 export default forwardRef(Youtube);
